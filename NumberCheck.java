e1a4fcacc9c382a70b5d82b476a615b3b8eadd2d
unnatial2005 <unnatilonakar@gmail.com>
2025-06-10 19:07:25 +0530

    main

---
+++ NumberCheck.java
@@ -0,0 +1,13 @@
+public class NumberCheck {
+    public static void main(String[] args) {
+        int num = 5; // Change this value to test with different numbers
+
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        } else if (num < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
