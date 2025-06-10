ceca5fd0fa298a2c81565517433553db9bad9a63
unnatial2005 <unnatilonakar@gmail.com>
2025-06-10 18:46:44 +0530

    save it

---
+++ SimpleInterest.java
@@ -0,0 +1,7 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        double p = 1000, r = 5, t = 2;
+        double si = (p * r * t) / 100;
+        System.out.println("Simple Interest: " + si);
+    }
+}
