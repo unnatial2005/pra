f3a116a2db0c9b15a93042e09a0d4e9b11d20d84
unnatial2005 <unnatilonakar@gmail.com>
2025-06-10 15:16:53 +0530

    save itt

---
+++ AgeValidator.java
@@ -0,0 +1,10 @@
+public class AgeValidator {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Valid Age: Eligible to vote");
+        } else {
+            System.out.println("Invalid Age: Not eligible to vote");
+        }
+    }
+}
